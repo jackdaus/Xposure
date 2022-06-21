9124a42d4b3e5795d55fabd96e42795476eb033f
Jack Daus <jackdaus@gmail.com>
2022-06-20 22:00:28 -0400

    add spider levels 0 - 3

--- Spider.cs
+++ Spider.cs
@@ -1,14 +1,24 @@
 ﻿using StereoKit;
+using System;
+using System.Collections.Generic;
 
 namespace StereoKitApp
 {
     public class Spider
     {
-        Pose cubePose = new Pose(0, -1.5f, -1.5f, Quat.Identity);
+        // TODO use floor height for y coord
+        Pose cubePose = new Pose(0, -1.45f, -1f, Quat.Identity);
         float rotateAngle = 0;
         Matrix translateMat = Matrix.T(0.5f, 0, 0);
         Model cube;
-        Model spiderModel;
+
+        int level = 0;
+        Model activeModel;
+        List<Model> spiderModels = new List<Model>();
+
+        private readonly Random _random = new Random();
+        bool isWalking = true;
+        DateTime lastWalkingChange;
 
         public Spider()
         {
@@ -16,24 +26,95 @@ namespace StereoKitApp
                 Mesh.GenerateRoundedCube(Vec3.One * 0.1f, 0.02f),
                 Default.MaterialUI);
 
-            spiderModel = Model.FromFile("spider/scene.gltf");
-            // Scale the spider
-            spiderModel.RootNode.ModelTransform *= Matrix.S(0.003f);
+            // initialize level 0 (empty model)
+            Model spiderModel = new Model();
+            spiderModels.Add(spiderModel);
 
+            // initialize level 1
+            spiderModel = Model.FromMesh(Mesh.GenerateSphere(0.05f), Default.MaterialUI);
+            spiderModels.Add(spiderModel);
+
+            // initialize level 2
+            spiderModel = Model.FromMesh(Mesh.GenerateSphere(0.05f), Default.MaterialUI);
+            spiderModel.RootNode.AddChild("test", Matrix.T(0, 0, -0.05f), Mesh.GenerateSphere(0.08f), Default.MaterialUI);
+            spiderModels.Add(spiderModel);
+
+            // initialize level 3
+            spiderModel = Model.FromFile("spider/scene.gltf");
+            spiderModel.RootNode.ModelTransform *= Matrix.S(0.003f); // Scale the spider
             spiderModel.PlayAnim("walk_ani_vor", AnimMode.Loop);
+            spiderModels.Add(spiderModel);
+            lastWalkingChange = DateTime.Now;
+
+            level = 3;
+            activeModel = spiderModels[level];
         }
 
         public void Step()
         {
-            rotateAngle -= 0.15f;
+            if (isWalking) rotateAngle -= 0.15f;
+
             var rotateMat = Matrix.R(0, rotateAngle, 0);
             var trMat = translateMat * rotateMat;
 
             // Create a UI handle cube where the model will be rooted. Useful for debugging
-            UI.Handle("Cube", ref cubePose, cube.Bounds);
-            cube.Draw(cubePose.ToMatrix());
-            
-            spiderModel.Draw(trMat * cubePose.ToMatrix());
+            //UI.Handle("Cube", ref cubePose, cube.Bounds);
+            //cube.Draw(cubePose.ToMatrix());
+
+            activeModel.Draw(trMat * cubePose.ToMatrix());
+
+            // randomly change walking status about every 1/300 steps, with a throttle of 3 seconds
+            var timeSinceLastChange = DateTime.Now - lastWalkingChange;
+            if (_random.Next(300) == 1 && timeSinceLastChange.TotalSeconds > 3)
+            {
+                // change animation for level 3
+                if (level == 3)
+                {
+                    if (isWalking)
+                        activeModel.PlayAnim("warte_pose", AnimMode.Loop);
+                    else
+                        activeModel.PlayAnim("walk_ani_vor", AnimMode.Loop);
+                }
+
+                isWalking = !isWalking;
+                lastWalkingChange = DateTime.Now;
+            }
+        }
+
+        /// <summary>
+        /// Set the current level of intensity for the spider
+        /// </summary>
+        /// <param name="level"></param>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        /// <exception cref="IndexOutOfRangeException"></exception>
+        public void setLevel(int level)
+        {
+            if (level < 0)
+                throw new ArgumentOutOfRangeException(nameof(level), "The level must be greater than or equal to 0");
+
+            if (level + 1 > spiderModels.Count)
+                throw new IndexOutOfRangeException($"The level {level} does not exist. The maximum level is {spiderModels.Count - 1}");
+
+            this.level = level;
+            activeModel = spiderModels[level];
+        }
+
+        /// <summary>
+        /// Get the current level of this spider
+        /// </summary>
+        /// <returns></returns>
+        public int getCurrentLevel()
+        {
+            return level;
+        }
+
+        /// <summary>
+        /// Get the maximum level
+        /// </summary>
+        /// <returns></returns>
+        public int getMaxLevel()
+        {
+            return spiderModels.Count - 1;
         }
     }
 }
